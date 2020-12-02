9518fe831a963ddb541ccd4f301b52ae3387c105
gruntbatch <gruntbatch@gmail.com>
2020-12-01 17:45:17 -0800

    Link edges to portals

--- tools/io_kowl/io_kowl.py
+++ tools/io_kowl/io_kowl.py
@@ -20,6 +20,7 @@ class ExportArea(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
         areas = list()
         parse_areas(context.scene.collection, areas)
         structure_areas(areas)
+        pprint.pprint(areas, compact=False, indent=2)
         export_areas(self.filepath, areas)
         return {"FINISHED"}
 
@@ -91,10 +92,9 @@ def tokenize(children):
 
 def structure_areas(areas):
     for area in areas:
-        # Navmesh
         area["navmesh"] = structure_navmesh(area["navmesh"])
-
-        # Portals
+        area["portals"] = structure_portals(area["portals"])
+        link_navmesh_to_portals(area["navmesh"], area["portals"])
 
 
 def structure_navmesh(obj):
@@ -103,19 +103,25 @@ def structure_navmesh(obj):
     bmesh.ops.triangulate(bm, faces=bm.faces)
     bmesh.ops.transform(bm, matrix=obj.matrix_world, verts=bm.verts)
 
-    triangles = dict()
+    triangles = list()
 
     if len(bm.faces) > 64:
         raise Exception("{} has more than 64 triangles".format(obj.name))
 
-    for face in bm.faces:
-        triangle = triangles.setdefault(face.index, list())
+    # I'm not actually sure that bm.faces is guaranteed to iterate in
+    # order of the face indices, so we perform a short test to enforce
+    # that it does
+    for i, face in enumerate(bm.faces):
+        if i != face.index:
+            raise Exception("`bm.faces` does not iterate in index order")
+
+        triangle = list()
 
         if len(face.loops) != 3:
             raise Exception("Triangulation failed")
         
         for loop in face.loops:
-            edge = dict({"vert": loop.vert.co.to_tuple()})
+            edge = dict({"vert": loop.vert.co})
 
             link_faces = loop.edge.link_faces
 
@@ -132,9 +138,53 @@ def structure_navmesh(obj):
 
             triangle.append(edge)
 
+        triangles.append(triangle)
+
     bm.free()
 
     return triangles
+
+
+def structure_portals(objs):
+    portals = list()
+
+    for obj in objs:
+        portals.append({
+            "transform": obj.matrix_world
+        })
+
+    return portals
+
+
+def link_navmesh_to_portals(navmesh, portals):
+    for triangle in navmesh:
+        for edge in range(3):
+            a = triangle[edge]
+            b = triangle[(edge + 1) % 3]
+
+            for i, portal in enumerate(portals):
+                transform = portal["transform"]
+                scale = transform.to_scale()
+
+                # Transform the vertices into local space
+                transform = transform.inverted()
+                va = transform @ a["vert"]
+                vb = transform @ b["vert"]
+
+                print(va, vb)
+
+                def in_box(p, b):
+                    return (-b.x <= p.x and p.x <= b.x
+                            and -b.y <= p.y and p.y <= b.y
+                            and -b.z <= p.z and p.z <= b.z)
+
+                if in_box(va, scale) and in_box(vb, scale):
+                    triangle[edge]["to"] = 2
+                    triangle[edge]["target"] = i
+                    if "edge" in portal:
+                        raise Exception("Already an edge!")
+                    else:
+                        portal["edge"] = edge
 
 
 def export_areas(filepath, areas):
@@ -144,12 +194,23 @@ def export_areas(filepath, areas):
         fw("# NAVIGATION\n")
         fw("\n")
 
+        # TODO Save each area to its own set of files
         for area in areas:
-            for triangle in area["navmesh"].values():
+            for i, triangle in enumerate(area["navmesh"]):
                 to = [x["to"] for x in triangle]
                 target = [x["target"] for x in triangle]
                 vert = [x["vert"] for x in triangle]
                 fw("triangle ")
+                fw("{} ".format(i))
                 fw("{},{},{} ".format(*to))
                 fw("{},{},{} ".format(*target))
-                fw("{} {} {}\n".format(*["{:.3f},{:.3f},{:.3f}".format(*x) for x in vert]))
+                fw("{} {} {}\n".format(*["{:.3f},{:.3f},{:.3f}".format(*x.to_tuple()) for x in vert]))
+            
+            fw("\n")
+            for i, portal in enumerate(area["portals"]):
+                p, r, s = portal["transform"].decompose()
+                fw("portal ")
+                fw("{} ".format(i))
+                fw("{} ".format(int(round(s.x))))
+                fw("{},{},{} ".format(*p.to_tuple()))
+                fw("{},{},{},{}\n".format(r.x, r.y, r.z, r.w))
